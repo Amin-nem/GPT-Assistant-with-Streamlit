afedc29131ad6a1647f2dd0d5abb6469341421c8
Amin-nem <132615975+Amin-nem@users.noreply.github.com>
2024-02-19 15:09:01 +0330

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ client = OpenAI()
 
 # Your chosen model
 #MODEL = "gpt-3.5-turbo-16k" # Legacy
-MODEL = "gpt-3.5-turbo-1106" # Latest model
+MODEL = "gpt-4-0125-preview" # Latest model
 #MODEL = "gpt-4-1106-preview"
 
 # Initialize session state variables
